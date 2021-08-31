ca3f3c0973d4b3014203f67d39e26a543fc9fab8
Shambhavi Aggarwal <agg.shambhavi@gmail.com>
2021-08-31 17:29:12 +0530

    add layers

---
+++ layers.py
@@ -0,0 +1,274 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+from torch.nn import Parameter
+import SpectralNorm
+
+# Normal ConvBlock
+class Conv2dLayer(nn.Module):
+    def __init__(
+        self,
+        in_channels,
+        out_channels,
+        kernel_size,
+        stride=1,
+        padding=0,
+        dilation=1,
+        pad_type="zero",
+        activation="elu",
+        norm="none",
+        sn="False",
+    ):
+        super().__init__()
+
+        #  Initialize the padiing scheme
+        if pad_type == "reflect":
+            self.pad = nn.ReflectionPad2d(padding)
+        elif pad_type == "replicate":
+            self.pad = nn.ReplicationPad2d(padding)
+        elif pad_type == "zero":
+            self.pad = nn.ZeroPad2d(padding)
+        else:
+            assert 0, "Unsupported padding type: {}".format(pad_type)
+
+        # Initialize the normalization type
+        if norm == "bn":
+            self.norm = nn.BatchNorm2d(out_channels)
+        elif self.norm == "in":
+            self.norm = nn.InstanceNorm2d(out_channels)
+        # skipping layer norm here, becuase i dont we are using it anywhere
+        elif norm == "none":
+            self.norm = None
+        else:
+            assert 0, "Unsupported normalization: {}".format(norm)
+
+        # Initialize the activation function
+        if activation == "relu":
+            self.activation = nn.ReLU(inplace=True)
+        elif activation == "lrelu":
+            self.activation = nn.LeakyReLU(0.2, inplace=True)
+        elif activation == "prelu":
+            self.activation = nn.PReLU()
+        elif activation == "selu":
+            self.activation = nn.SELU(inplace=True)
+        elif activation == "tanh":
+            self.activation = nn.Tanh()
+        elif activation == "sigmoid":
+            self.activation = nn.Sigmoid()
+        elif activation == "none":
+            self.activation = None
+        else:
+            assert 0, "Unsupported activation: {}".format(activation)
+
+        # Initialize the conv layer
+        if sn:
+            self.conv2d = SpectralNorm(
+                nn.Conv2d(
+                    in_channels=in_channels,
+                    out_channels=out_channels,
+                    kernel_size=kernel_size,
+                    stride=stride,
+                    padding=padding,
+                    dilation=dilation,
+                )
+            )
+        else:
+            self.conv2d = nn.Conv2d(
+                in_channels=in_channels,
+                out_channels=out_channels,
+                kernel_size=kernel_size,
+                stride=stride,
+                padding=padding,
+                dilation=dilation,
+            )
+
+    def forward(self, x):
+        x = self.pad(x)
+        x = self.conv2d(x)
+        if self.norm:
+            x = self.norm(x)
+        if self.activation:
+            x = self.activation(x)
+        return x
+
+
+# Transpose ConvBlock
+class TransposeConv2dLayer(nn.Module):
+    def __init__(
+        self,
+        in_channels,
+        out_channels,
+        kernel_size,
+        stride=1,
+        padding=0,
+        dilation=1,
+        pad_type="zero",
+        activation="lrelu",
+        norm="none",
+        sn=False,
+        scale_factor=2,
+    ):
+        super().__init__()
+        self.scale_factor = scale_factor
+        self.conv2d = Conv2dLayer(
+            in_channels,
+            out_channels,
+            kernel_size,
+            stride,
+            padding,
+            dilation,
+            pad_type,
+            activation,
+            norm,
+            sn,
+        )
+
+    def forward(self, x):
+        x = F.interpolate(x, scale_factor=self.scale_factor, mode="nearest")
+        return self.conv2d(x)
+
+
+# Gated ConvBlock
+class GatedConv2dLayer(nn.Module):
+    def __init__(
+        self,
+        in_channels,
+        out_channels,
+        kernel_size,
+        stride=1,
+        padding=0,
+        dilation=1,
+        pad_type="zero",
+        activation="elu",
+        norm="none",
+        sn="False",
+    ):
+        super().__init__()
+
+        #  Initialize the padiing scheme
+        if pad_type == "reflect":
+            self.pad = nn.ReflectionPad2d(padding)
+        elif pad_type == "replicate":
+            self.pad = nn.ReplicationPad2d(padding)
+        elif pad_type == "zero":
+            self.pad = nn.ZeroPad2d(padding)
+        else:
+            assert 0, "Unsupported padding type: {}".format(pad_type)
+
+        # Initialize the normalization type
+        if norm == "bn":
+            self.norm = nn.BatchNorm2d(out_channels)
+        elif self.norm == "in":
+            self.norm = nn.InstanceNorm2d(out_channels)
+        # skipping layer norm here, becuase i dont we are using it anywhere
+        elif norm == "none":
+            self.norm = None
+        else:
+            assert 0, "Unsupported normalization: {}".format(norm)
+
+        # Initialize the activation function
+        if activation == "relu":
+            self.activation = nn.ReLU(inplace=True)
+        elif activation == "lrelu":
+            self.activation = nn.LeakyReLU(0.2, inplace=True)
+        elif activation == "prelu":
+            self.activation = nn.PReLU()
+        elif activation == "selu":
+            self.activation = nn.SELU(inplace=True)
+        elif activation == "tanh":
+            self.activation = nn.Tanh()
+        elif activation == "sigmoid":
+            self.activation = nn.Sigmoid()
+        elif activation == "none":
+            self.activation = None
+        else:
+            assert 0, "Unsupported activation: {}".format(activation)
+
+        # Initialize the conv layer
+        if sn:
+            self.conv2d = SpectralNorm(
+                nn.Conv2d(
+                    in_channels=in_channels,
+                    out_channels=out_channels,
+                    kernel_size=kernel_size,
+                    stride=stride,
+                    padding=padding,
+                    dilation=dilation,
+                )
+            )
+            self.mask_conv2d = SpectralNorm(
+                nn.Conv2d(
+                    in_channels=in_channels,
+                    out_channels=out_channels,
+                    kernel_size=kernel_size,
+                    stride=stride,
+                    padding=padding,
+                    dilation=dilation,
+                )
+            )
+        else:
+            self.conv2d = nn.Conv2d(
+                in_channels=in_channels,
+                out_channels=out_channels,
+                kernel_size=kernel_size,
+                stride=stride,
+                padding=padding,
+                dilation=dilation,
+            )
+            self.mask_conv2d = nn.Conv2d(
+                in_channels=in_channels,
+                out_channels=out_channels,
+                kernel_size=kernel_size,
+                stride=stride,
+                padding=padding,
+                dilation=dilation,
+            )
+
+    def forward(self, x):
+        x = self.pad(x)
+        conv = self.conv2d(x)
+        mask = self.mask_conv2d(x)
+        gated_mask = self.sigmoid(mask)
+        x = conv * gated_mask
+        if self.norm:
+            x = self.norm(x)
+        if self.activation:
+            x = self.activation(x)
+        return x
+
+
+# Transpose GatedConvBlock
+class TransposeGatedConv2dLayer(nn.Module):
+    def __init__(
+        self,
+        in_channels,
+        out_channels,
+        kernel_size,
+        stride=1,
+        padding=0,
+        dilation=1,
+        pad_type="zero",
+        activation="lrelu",
+        norm="none",
+        sn=False,
+        scale_factor=2,
+    ):
+        super().__init__()
+        self.scale_factor = scale_factor
+        self.gated_conv2d = GatedConv2dLayer(
+            in_channels,
+            out_channels,
+            kernel_size,
+            stride,
+            padding,
+            dilation,
+            pad_type,
+            activation,
+            norm,
+            sn,
+            scale_factor=2,
+        )
+
+    def forward(self, x):
+        x = F.interpolate(x, scale_factor=self.scale_factor, mode="nearest")
+        return self.gated_conv2d(x)
