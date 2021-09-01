4cd83c39d86fdbe5af39dad968a5a7ec1e30cd13
Shambhavi Aggarwal <agg.shambhavi@gmail.com>
2021-09-01 20:58:58 +0530

    add perceptual net (vgg)

--- network.py
+++ network.py
@@ -505,3 +505,36 @@ class PatchDiscriminator(nn.Module):
         x = self.block5(x)
         x = self.block6(x)
         return x
+    
+# Perceptual Network
+# VGG-16 conv4_3 features
+class PerceptualNet(nn.Module):
+    def __init__(self):
+        super(PerceptualNet, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, 3, 1, 1),
+            nn.ReLU(inplace = True),
+            nn.Conv2d(64, 64, 3, 1, 1),
+            nn.ReLU(inplace = True),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(64, 128, 3, 1, 1),
+            nn.ReLU(inplace = True),
+            nn.Conv2d(128, 128, 3, 1, 1),
+            nn.ReLU(inplace = True),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(128, 256, 3, 1, 1),
+            nn.ReLU(inplace = True),
+            nn.Conv2d(256, 256, 3, 1, 1),
+            nn.ReLU(inplace = True),
+            nn.Conv2d(256, 256, 3, 1, 1),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(256, 512, 3, 1, 1),
+            nn.ReLU(inplace = True),
+            nn.Conv2d(512, 512, 3, 1, 1),
+            nn.ReLU(inplace = True),
+            nn.Conv2d(512, 512, 3, 1, 1)
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        return x
